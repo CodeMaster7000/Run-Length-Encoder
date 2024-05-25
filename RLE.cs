9ccc40a05298f46bf2bf866cdd7255e21dde64aa
CodeMaster7000 <abhyankar.advait19@gmail.com>
2024-05-25 22:16:24 +0100

    Update RLE.cs

--- RLE.cs
+++ RLE.cs
@@ -1,1 +1,15 @@
+Console.Write("Enter string of data to compress (RLE): ");
+string rawData = Console.ReadLine();
 
+for (int letterNum = 0; letterNum < rawData.Length; letterNum++)
+{
+    var num = 1;
+    while (letterNum < rawData.Length - 1 && rawData[letterNum] == rawData[letterNum + 1])
+    {
+
+        num++;
+        letterNum++;
+
+    }
+    Console.Write($"{num}{rawData[letterNum]}");
+}
